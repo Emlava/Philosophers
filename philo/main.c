a204c4bf105e6a806d89b1ab4b2baf7bbff769bc
Emilio Lara Vargas <elara-va@mi-r4-p3.s19.be>
2025-11-28 16:46:25 +0100

    .

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,20 @@
 /*   By: elara-va <elara-va@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 20:55:38 by emlava            #+#    #+#             */
-/*   Updated: 2025/11/27 21:03:03 by elara-va         ###   ########.fr       */
+/*   Updated: 2025/11/28 16:45:33 by elara-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+int main(int ac, char *av[])
+{
+	pthread_mutex_t	*forks;
+	
+	if (ac < 5 || ac > 6)
+	{
+		write(2, "Incorrect number of arguments\n", 30);
+		return (1);
+	}
+	forks = malloc(sizeof(pthread_mutex_t) * ft_atoi(av[1]));
+}
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,20 @@
 /*   By: elara-va <elara-va@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 21:18:57 by elara-va          #+#    #+#             */
-/*   Updated: 2025/11/27 21:18:58 by elara-va         ###   ########.fr       */
+/*   Updated: 2025/11/28 16:43:47 by elara-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+/**** atoi.c ****/
+int	ft_atoi(const char *nptr);
+
+#endif
