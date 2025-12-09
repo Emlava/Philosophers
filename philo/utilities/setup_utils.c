fd27ffecbc43157a1af7d8d8153fafb35f6b24eb
Emilio Lara <emiliolaravargas@hotmail.com>
2025-12-09 23:56:34 +0100

    .

--- philo/utilities/setup_utils.c
+++ philo/utilities/setup_utils.c
@@ -12,6 +12,40 @@
 
 #include "../philo.h"
 
+static size_t	ft_if_space_or_sign(const char *nptr)
+{
+	size_t	i;
+
+	i = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+		i++;
+	return (i);
+}
+
+static int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		minus;
+	long	result;
+
+	minus = 0;
+	result = 0;
+	i = ft_if_space_or_sign(nptr);
+	if (i && nptr[i - 1] == '-')
+		minus = 1;
+	if (nptr[i] < '0' || nptr[i] > '9')
+		return (0);
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	if (minus)
+		result = -result;
+	return ((int)result);
+
 int	convert_args_to_int(char *av[], t_resources *resources, int ac)
 {
 	resources->nbr_of_philos = ft_atoi(av[1]);
